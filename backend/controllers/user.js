9df853bc036a40b391c033cde5c9238f3a65bc21
Max Turon <maxx@turon.fr>
2021-08-12 16:14:33 +0200

    Création d'un utilisateur OK.

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,49 +1,31 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const db = require("../models");
 
-const User = require('../models/user');
-
-exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User({
-                email: req.body.email,
-                password: hash,
-                admin: 0,
-            });
-            console.log(user);
-            user.save()
-                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-                .catch(error => res.status(400).json('Test erreur'));
-        })
-        .catch(error => res.status(500).json({ error }));
-};
-
-
-exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            //Vérification de l'email
-            if (!user) {
-                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+exports.signup = (req, res) => {
+    db.user.findOne({
+        where: { email: req.body.email },
+    })
+        .then((userFound) => {
+            if (!userFound) {
+                bcrypt.hash(req.body.password, 10).then((hash) => {
+                    db.user.create({
+                        email: req.body.email,
+                        password: hash,
+                        admin: 0,
+                    })
+                        .then((user) => {
+                            res.status(201).json({
+                                id: user.id,
+                            });
+                        })
+                        .catch((error) => res.status(400).json({ error }));
+                });
+            } else {
+                res.status(409).json({
+                    error: "Cet utilisateur existe déjà",
+                });
             }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    //Vérification du mdp
-                    if (!valid){
-                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
-                    }
-                    //Génération du Token
-                    res.status(200).json({userId: user._id, token: jwt.sign(
-                            //ID utilisateur
-                            { userId: user._id },
-                            //Chaîne secrète de développement temporaire
-                            'RANDOM_TOKEN_SECRET',
-                            //Défini la durée du token
-                            { expiresIn: '24h' })
-                    });
-                })
-                .catch(error => res.status(500).json({ error }));
         })
-        .catch(error => res.status(500).json({ error }));
+        .catch((error) => res.status(500).json({ error }));
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,5 @@ const router = express.Router();
 const userCtrl = require('../controllers/user');
 
 router.post('/signup', userCtrl.signup); // Création d'un compte
-router.post('/login', userCtrl.login); // Connexion à un compte
 
 module.exports = router;  //Export vers app.js
